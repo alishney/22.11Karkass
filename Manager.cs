c54281562bf5b53c2bd99537178e66b3982676e9
Alina <80460626+alishney@users.noreply.github.com>
2021-11-22 14:27:16 +0300

    Initial commit

---
+++ Manager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace _22._11Karkass
+{
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+
+    }
+}
